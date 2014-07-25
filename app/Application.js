d4127b12b4824003087f061dc2447df125812026
makotolusi <makoto.deadpool@gmail.com>
2014-07-25 17:37:13 +0800

    first

---
+++ app/Application.js
@@ -0,0 +1,53 @@
+/**
+ * The main application class. An instance of this class is created by app.js when it calls
+ * Ext.application(). This is the ideal place to handle application launch and initialization
+ * details.
+ */
+Ext.define('Push.Application', {
+	extend : 'Ext.app.Application',
+	name : 'Push',
+	namespace : 'Push',
+
+	requires : [
+	    'Push.view.login.Login'],
+
+	views : [
+'login.Login',
+	'app.AppList', 'push.PushList', 
+	'navigation.Breadcrumb', 'Header', 'ContentPanel', 'navigation.Tree', 'grid.ArrayGrid', 'grid.Paging', 'grid.GridPlugins', 'form.HBoxLayoutForm'
+	],
+
+	controllers : [
+	'Global', 'Root'
+	// TODO: add controllers here
+	],
+
+	stores : [],
+
+	init : function() {
+
+        
+             var session = this.session = new Ext.data.Session();
+        
+        this.login = new Push.view.login.Login({
+            session: session,
+            autoShow: true
+        });
+		// if ('nocss3' in Ext.Object.fromQueryString(location.search)) {
+			// Ext.supports.CSS3BorderRadius = false;
+			// Ext.getBody().addCls('x-nbr x-nlg');
+		// }
+// 
+		var me = this, map = Ext.Object.fromQueryString(location.search), charts = ('charts' in map) && !/0|false|no/i.test(map.charts);
+		Ext.create('Push.store.Navigation', {
+			storeId : 'navigation'
+		});
+// 
+		// Set the default route to start the application.
+		me.setDefaultToken('all');
+// 
+		// Ext.setGlyphFontFamily('Pictos');
+		// Ext.tip.QuickTipManager.init();
+		// Ext.state.Manager.setProvider(Ext.create('Ext.state.CookieProvider'));
+	}
+});
---
+++ app/controller/Root.js
@@ -0,0 +1,26 @@
+/**
+ * The main application controller. This is a good place to handle things like routes.
+ */
+Ext.define('Push.controller.Root', {
+	extend : 'Ext.app.Controller',
+	requires : ['Push.view.*'],
+	stores : ['Companies'],
+	config : {
+		control : {
+			'#enter' : {
+				handler : 'onEnterApp'
+			}
+		},
+		refs : {
+			viewport : 'viewport',
+			contentPanel : 'contentPanel'
+		},
+		routes : {
+		}
+	},
+	onEnterApp : function(data) {
+		Ext.widget('array-grid',{a:'111'});
+		this.redirectTo('array-grid');
+		
+	},
+});
